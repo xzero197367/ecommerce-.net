44f486e7453647114eec84dcc2ccd6de4d8a7a17
peter77367 <peterzaky77@gmail.com>
2025-05-05 15:46:18 +0300

    cartitem add

---
+++ Ecommerce.Models/CartItem.cs
@@ -0,0 +1,18 @@
+
+namespace Ecommerce.Models
+{
+    public class CartItem
+    {
+        public int CartItemID { get; set; }
+
+        public int UserID { get; set; }
+        //public User User { get; set; }
+
+        public int ProductID { get; set; }
+        //public Product Product { get; set; }
+
+        public int Quantity { get; set; }
+        public DateTime DateAdded { get; set; }
+    }
+
+}
