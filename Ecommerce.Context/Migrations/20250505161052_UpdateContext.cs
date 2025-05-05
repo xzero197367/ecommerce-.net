c112538f74b6c9c3c3ddd2b5de41b9ad87706c63
Alaa abd el hady zaky <alaa20-01773@student.eelu.edu.eg>
2025-05-05 19:12:27 +0300

    Update Context

---
+++ Ecommerce.Context/Migrations/20250505161052_UpdateContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecommerce.Context.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserID",
+                table: "products",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_products_UserID",
+                table: "products",
+                column: "UserID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_products_User_UserID",
+                table: "products",
+                column: "UserID",
+                principalTable: "User",
+                principalColumn: "UserID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_products_User_UserID",
+                table: "products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_products_UserID",
+                table: "products");
+
+            migrationBuilder.DropColumn(
+                name: "UserID",
+                table: "products");
+        }
+    }
+}
